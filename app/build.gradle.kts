ab23f6e9346f42e7c7da82961b36344c424c0900
Mudinlah <dinmulah@gmail.com>
2026-04-13 23:20:38 +0700

    Membersihkan warning project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,12 @@ android {
     buildFeatures {
         compose = true
     }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/versions/9/previous-compilation-data.bin"
+        }
+    }
 }
 
 dependencies {
