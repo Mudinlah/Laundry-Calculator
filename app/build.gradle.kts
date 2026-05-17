ddde8144d4a6d6d24bca456f2a992c3243a12de5
Mudinlah <dinmulah@gmail.com>
2026-05-18 03:05:07 +0700

    Menambah Library Room, ksp, dan datastorePersistentLibrary

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.devtools.ksp)
 }
 
 android {
@@ -58,6 +59,12 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.compose.material.icons)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.compose.runtime.livedata)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.datastore.preferences)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
